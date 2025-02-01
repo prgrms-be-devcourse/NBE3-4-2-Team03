6a8c3a165058bb7029b708a5ef59f3f607e03050
leey311 <wwyl1@naver.com>
2025-02-01 20:14:59 +0900

    test: 테스트케이스 추가

--- backend/src/test/java/com/programmers/pcquotation/PcquotationApplicationTests.java
+++ backend/src/test/java/com/programmers/pcquotation/PcquotationApplicationTests.java
@@ -1,13 +1,32 @@
 package com.programmers.pcquotation;
 
+import com.programmers.pcquotation.estimaterequest.entity.EstimateRequest;
+import com.programmers.pcquotation.estimaterequest.repository.EstimateRequestRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class PcquotationApplicationTests {
+    @Autowired
+    public EstimateRequestRepository estimateRequestRepository;
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+        estimateRequestRepository.save(EstimateRequest
+                .builder()
+                .purpose("qwe")
+                .budget(100)
+                .otherRequest("asd")
+                .createDate(LocalDateTime.now())
+                .build());
+
+
+        assertThat(estimateRequestRepository.findAll().size()).isEqualTo(1);
+    }
 
 }
