87aedaadac17f3b9cc5df079afea27f79fef63b9
류대현 <eogus1027@naver.com>
2025-02-03 15:20:24 +0900

    #11 chore - 중복되는 security config 삭제 및 테스트 코드 위치 이동

--- backend/src/test/java/com/programmers/pcquotation/customers/controller/CustomerAuthControllerTest.java
+++ backend/src/test/java/com/programmers/pcquotation/customers/controller/CustomerAuthControllerTest.java
@@ -1,4 +1,4 @@
-package com.programmers.pcquotation;
+package com.programmers.pcquotation.customers.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
