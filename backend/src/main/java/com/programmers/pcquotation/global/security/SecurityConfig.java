3a780de68fb10d6bdf682dde832187aa20c6a2a8
UCLABgown <lovecein4858@gmail.com>
2025-02-03 14:53:23 +0900

    #8 refactor - 시큐리티 컨피그 수정

--- backend/src/main/java/com/programmers/pcquotation/global/security/SecurityConfig.java
+++ backend/src/main/java/com/programmers/pcquotation/global/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
@@ -15,11 +17,16 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(authorizeRequests ->
 				authorizeRequests
-					.requestMatchers("/h2-console/**").permitAll()
 					.anyRequest().permitAll()
 			)
 			.csrf(AbstractHttpConfigurer::disable);
 
 		return http.build();
 	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 }
