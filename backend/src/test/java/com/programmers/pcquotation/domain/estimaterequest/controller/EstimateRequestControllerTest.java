c2bbf03cf7ff7d6a268b440aada7e12c65659f87
leey311 <wwyl1@naver.com>
2025-02-04 11:48:34 +0900

    fix -충돌처리

---
+++ backend/src/test/java/com/programmers/pcquotation/domain/estimaterequest/controller/EstimateRequestControllerTest.java
@@ -0,0 +1,15 @@
+package com.programmers.pcquotation.domain.estimaterequest.controller;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest
+class EstimateRequestControllerTest {
+
+    @Test
+    @Transactional
+    void 견적_요청_생성() {
+
+    }
+}
