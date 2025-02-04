ae01b59125b682db7a9e6ae6db4168a4d3e3067d
Daewook123 <cdo5080@gmail.com>
2025-02-04 12:45:04 +0900

    pull 하기전  커밋2

--- backend/src/main/java/com/programmers/pcquotation/domain/categories/dto/CategoryCreateRequest.java
+++ backend/src/main/java/com/programmers/pcquotation/domain/categories/dto/CategoryCreateRequest.java
@@ -3,8 +3,6 @@ package com.programmers.pcquotation.domain.categories.dto;
 import jakarta.validation.constraints.NotEmpty;
 
 public record CategoryCreateRequest(
-
-	@NotEmpty
-	String category
+	@NotEmpty String category
 ) {
 }
