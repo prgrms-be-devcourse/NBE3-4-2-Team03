9e26d6ff81109c8fd04ab10d3d857c9da9d7a5ac
UCLABgown <lovecein4858@gmail.com>
2025-02-04 12:50:31 +0900

    #45 feat - 토큰 서비스 추가 / jwt처리 클라스 추가

---
+++ backend/src/main/java/com/programmers/pcquotation/domain/sellers/service/AuthTokenService.java
@@ -0,0 +1,41 @@
+package com.programmers.pcquotation.domain.sellers.service;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.programmers.pcquotation.domain.sellers.entitiy.Sellers;
+import com.programmers.pcquotation.global.jwt.Jwt;
+
+@Service
+public class AuthTokenService {
+	@Value("${custom.jwt.secretKey}")
+	private String jwtSecretKey;
+
+	@Value("${custom.accessToken.expirationSeconds}")
+	private long accessTokenExpirationSeconds;
+
+	String getAccessToken(Sellers sellers) {
+		long id = sellers.getId();
+		String username = sellers.getUsername();
+
+		return Jwt.toString(
+			jwtSecretKey,
+			accessTokenExpirationSeconds,
+			Map.of("id", id, "username", username)
+		);
+	}
+
+	Map<String, Object> payload(String accessToken) {
+		Map<String, Object> parsedPayload = Jwt.payload(jwtSecretKey, accessToken);
+
+		if(parsedPayload == null) return null;
+
+		long id = (long) (Integer) parsedPayload.get("id");
+		String username = (String) parsedPayload.get("username");
+
+		return Map.of("id", id, "username", username);
+	}
+}
+
---
+++ backend/src/main/java/com/programmers/pcquotation/global/jwt/Jwt.java
@@ -0,0 +1,59 @@
+package com.programmers.pcquotation.global.jwt;
+
+import java.util.Date;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+public class Jwt {
+	public static String toString(String secret, long expireSeconds, Map<String, Object> body) {
+		Date issuedAt = new Date();
+		Date expiration = new Date(issuedAt.getTime() + 1000L * expireSeconds);
+
+		SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+
+		String jwt = Jwts.builder()
+			.claims(body)
+			.issuedAt(issuedAt)
+			.expiration(expiration)
+			.signWith(secretKey)
+			.compact();
+
+		return jwt;
+	}
+
+	public static boolean isValid(String secret, String jwtStr) {
+		SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+
+		try {
+			Jwts
+				.parser()
+				.verifyWith(secretKey)
+				.build()
+				.parse(jwtStr);
+		} catch (Exception e) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public static Map<String, Object> payload(String secret, String jwtStr) {
+		SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+
+		try {
+			return (Map<String, Object>) Jwts
+				.parser()
+				.verifyWith(secretKey)
+				.build()
+				.parse(jwtStr)
+				.getPayload();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+}
