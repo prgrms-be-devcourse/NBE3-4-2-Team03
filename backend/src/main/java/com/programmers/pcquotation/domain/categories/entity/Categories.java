7b5d822faac168b8a6f5cfed84b5b7744c0992d9
junho3168 <junho3168@naver.com>
2025-02-04 12:37:18 +0900

    feature - 카테고리와 부품 엔티티 연결

--- backend/src/main/java/com/programmers/pcquotation/domain/categories/entity/Categories.java
+++ backend/src/main/java/com/programmers/pcquotation/domain/categories/entity/Categories.java
@@ -1,9 +1,16 @@
 package com.programmers.pcquotation.domain.categories.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.programmers.pcquotation.domain.items.entity.Items;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,6 +27,9 @@ public class Categories {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String category;
+
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Items> items = new ArrayList<>();
 
 	public void updateCategory(String category) {
 		this.category = category;
