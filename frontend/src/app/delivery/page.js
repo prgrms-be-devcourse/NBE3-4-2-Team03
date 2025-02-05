5ea99c3adf88908e95d446ff0259bce1af045015
leey311 <wwyl1@naver.com>
2025-02-05 15:22:37 +0900

    fix -프론트수정

---
+++ frontend/src/app/delivery/page.js
@@ -0,0 +1,11 @@
+// pages/index.js
+import DeliveryForm from './components/DeliveryForm';
+
+const Home = () => (
+    <div>
+        <h1>Create Delivery</h1>
+        <DeliveryForm />
+    </div>
+);
+
+export default Home;
