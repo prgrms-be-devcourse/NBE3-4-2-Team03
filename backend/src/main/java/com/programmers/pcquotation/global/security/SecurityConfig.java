22de2d5cee73a2f8e16f072b10d7e6daf9b50a95
junho3168 <junho3168@naver.com>
2025-02-06 13:00:28 +0900

    feature - cors 설정 추가

--- backend/src/main/java/com/programmers/pcquotation/global/security/SecurityConfig.java
+++ backend/src/main/java/com/programmers/pcquotation/global/security/SecurityConfig.java
@@ -1,15 +1,18 @@
 package com.programmers.pcquotation.global.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,13 +25,12 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(authorizeRequests ->
 				authorizeRequests
-					.requestMatchers(HttpMethod.GET, "/seller/api/**")
-					.hasRole("SELLER")
-					.requestMatchers(HttpMethod.GET, "/seller")
+					.requestMatchers(HttpMethod.GET, "/sellers")
 					.hasRole("SELLER")
 					.anyRequest()
 					.permitAll()
 			)
+			.cors(cors -> corsConfigurationSource())
 			.csrf(AbstractHttpConfigurer::disable)
 			.sessionManagement(sessionManagementConfigurer ->
 				sessionManagementConfigurer
@@ -61,4 +63,18 @@ public class SecurityConfig {
 		return http.build();
 	}
 
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(List.of("http://localhost:3000"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setExposedHeaders(List.of("*"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
 }
