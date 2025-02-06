802edca492698cffa213f8fff7106839be3d3e84
UCLABgown <lovecein4858@gmail.com>
2025-02-06 17:33:25 +0900

    #80 feat - cors 해결

--- backend/src/main/java/com/programmers/pcquotation/global/security/SecurityConfig.java
+++ backend/src/main/java/com/programmers/pcquotation/global/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.programmers.pcquotation.global.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,6 +12,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -60,5 +64,26 @@ public class SecurityConfig {
 
 		return http.build();
 	}
+	@Bean
+	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
 
+		// 허용할 오리진 설정
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+
+		// 허용할 HTTP 메서드 설정
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+
+		// 자격 증명 허용 설정
+		configuration.setAllowCredentials(true);
+
+		// 허용할 헤더 설정
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+
+		// CORS 설정을 소스에 등록
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
+	}
 }
