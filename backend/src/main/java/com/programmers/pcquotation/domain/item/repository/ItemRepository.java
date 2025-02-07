c9c7db2e55bcb2f136b389202e834f73bc330e66
junho3168 <junho3168@naver.com>
2025-02-07 14:49:28 +0900

    feature - 부품 카테고리별로 조회할 수 있게 메서드 추가 및 컨트롤러 수정

--- backend/src/main/java/com/programmers/pcquotation/domain/item/repository/ItemRepository.java
+++ backend/src/main/java/com/programmers/pcquotation/domain/item/repository/ItemRepository.java
@@ -1,5 +1,7 @@
 package com.programmers.pcquotation.domain.item.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.programmers.pcquotation.domain.item.entity.Item;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+	List<Item> findByCategoryId(Long categoryId);
 }
--- backend/src/main/java/com/programmers/pcquotation/domain/item/service/ItemService.java
+++ backend/src/main/java/com/programmers/pcquotation/domain/item/service/ItemService.java
@@ -52,7 +52,7 @@ public class ItemService {
 		return new ItemCreateResponse(savedItem.getId(), "부품 생성 완료");
 	}
 
-	//부품 조회
+	//모든 부품 조회
 	@Transactional
 	public List<ItemInfoResponse> getItemList() {
 		List<Item> items = itemRepository.findAll();
@@ -63,6 +63,21 @@ public class ItemService {
 				item.getCategory().getId(),         // 카테고리 ID
 				item.getCategory().getCategory(),   // 카테고리 이름 (categoryName)
 				item.getImgFilename()               // 이미지 파일명
+			))
+			.collect(Collectors.toList());
+	}
+
+	//특정 카테고리의 아이템만 조회
+	@Transactional
+	public List<ItemInfoResponse> getItemsByCategory(Long categoryId) {
+		List<Item> items = itemRepository.findByCategoryId(categoryId); // JPA에서 categoryId로 조회
+		return items.stream()
+			.map(item -> new ItemInfoResponse(
+				item.getId(),
+				item.getName(),
+				item.getCategory().getId(),
+				item.getCategory().getCategory(),
+				item.getImgFilename()
 			))
 			.collect(Collectors.toList());
 	}
