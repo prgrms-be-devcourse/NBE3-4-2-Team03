82b4e5bfa44621afa108ec07abf6ac1d34d50d3a
junho3168 <junho3168@naver.com>
2025-02-07 15:03:24 +0900

    fix - joinfetch로 n+1 방지

--- backend/src/main/java/com/programmers/pcquotation/domain/item/repository/ItemRepository.java
+++ backend/src/main/java/com/programmers/pcquotation/domain/item/repository/ItemRepository.java
@@ -3,11 +3,13 @@ package com.programmers.pcquotation.domain.item.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.programmers.pcquotation.domain.item.entity.Item;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+	@Query("SELECT i FROM Item i JOIN FETCH i.category WHERE i.category.id = :categoryId")
 	List<Item> findByCategoryId(Long categoryId);
 }
