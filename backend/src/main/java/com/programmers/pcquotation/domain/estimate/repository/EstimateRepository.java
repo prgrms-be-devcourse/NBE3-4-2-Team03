0af8e6726668a0844932e4d477436e2a891841b5
junho3168 <junho3168@naver.com>
2025-02-07 11:23:52 +0900

    fix - 아이템 삭제 시 발생하는 외래 키 제약 오류 수정

--- backend/src/main/java/com/programmers/pcquotation/domain/estimate/repository/EstimateRepository.java
+++ backend/src/main/java/com/programmers/pcquotation/domain/estimate/repository/EstimateRepository.java
@@ -3,9 +3,17 @@ package com.programmers.pcquotation.domain.estimate.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.programmers.pcquotation.domain.estimate.entity.Estimate;
 
 public interface EstimateRepository extends JpaRepository<Estimate, Integer> {
 	List<Estimate> getAllByEstimateRequest_Id(Integer estimateRequestId);
+
+	@Modifying
+	@Query("DELETE FROM EstimateComponent ec WHERE ec.item.id = :itemId")
+	void deleteComponentsByItemId(@Param("itemId") Long itemId);
 }
+
