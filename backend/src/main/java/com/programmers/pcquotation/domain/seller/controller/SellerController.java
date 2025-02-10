f0846bb6ce44e79da6d5b904067801584211a656
류대현 <eogus1027@naver.com>
2025-02-10 10:47:53 +0900

    #94 fix - 판매자 회원정보가 제대로 표시되도록 수정

--- backend/src/main/java/com/programmers/pcquotation/domain/seller/controller/SellerController.java
+++ backend/src/main/java/com/programmers/pcquotation/domain/seller/controller/SellerController.java
@@ -1,16 +1,11 @@
 package com.programmers.pcquotation.domain.seller.controller;
 
+import java.security.Principal;
 import java.util.NoSuchElementException;
 
 import org.springframework.lang.NonNull;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.programmers.pcquotation.domain.member.entitiy.Member;
 import com.programmers.pcquotation.domain.seller.dto.SellerInfoRespnse;
@@ -35,24 +30,23 @@ public class SellerController {
 
 	@GetMapping
 	@Transactional(readOnly = true)
-	public SellerInfoRespnse info() {
-		Seller seller = (Seller)rq.getMember();
-		if (seller == null)
-			throw new NullPointerException("존재하지 않는 사용자입니다.");
+	public SellerInfoRespnse info(Principal principal) {
+		Seller seller = sellerService
+				.findByUserName(principal.getName())
+				.orElseThrow(() -> new NullPointerException("존재하지 않는 사용자입니다."));
+
 		return SellerInfoRespnse.builder()
 			.id(seller.getId())
 			.username(seller.getUsername())
 			.companyName(seller.getCompanyName())
 			.email(seller.getEmail()).build();
-
 	}
 
-
 	@PutMapping
-	public String modify(@RequestBody @Valid SellerUpdateDto customerUpdateDto) {
-		Seller seller = (Seller)rq.getMember();
-		if(seller == null)
-			throw new NoSuchElementException("존재하지 않는 사용자입니다.");
+	public String modify(@RequestBody @Valid SellerUpdateDto customerUpdateDto, Principal principal) {
+		Seller seller = sellerService
+				.findByUserName(principal.getName())
+				.orElseThrow(() -> new NullPointerException("존재하지 않는 사용자입니다."));
 
 		sellerService.modify(seller, customerUpdateDto);
 		return "정보수정이 성공했습니다.";
